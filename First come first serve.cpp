e840060b593a275729ff79873c838d34c0170cc1
SarthakPardeshi <146865265+SarthakPardeshi@users.noreply.github.com>
2025-11-10 14:28:15 +0530

    Create First come first serve.cpp

---
+++ First come first serve.cpp
@@ -0,0 +1,28 @@
+BLACK
+NOVEMBER
+Get 66% off PRO
+Are you struggling to build your coding confidence or land your first job?
+Fast-track to your first pay-check.
+Ends in
+21h:02m:23s
+Start PRO
+
+C++ Online Compiler
+Programiz PRO ❯
+main.cpp
+
+
+Share
+
+ Run 
+}
+
+
+
+ 
+Output
+ Clear 
+=== Code Execution Successful ===
+
+
+ 
